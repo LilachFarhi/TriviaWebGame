95c3c08298496d249173246fdc6f100296e95a65
mayasb <mayasb9@gmail.com>
2014-12-20 23:01:26 +0200

    similargames

--- src/java/servlets/NewQuestion.java
+++ src/java/servlets/NewQuestion.java
@@ -12,25 +12,74 @@ public class NewQuestion extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String type = response.getHeader("questionType");
+        String type = request.getParameter("questionType");
+        
+        if (type == null)
+        {
+            type = "";
+        }
+        
+        boolean isValid = true;
+        String answerText;
+        
          switch (type) {
             case "Open":  
-                     break;
-            case "MultipleAnswer":  
-                     break;
+                answerText = "<input type=\"text\" name=\"answer\" width=\"100\" height=\"100\">";
+                answerText += "<br>";
+                break;
+            case "MultipleAnswer":
+                answerText = "<input type=\"text\" name=\"answer\" width=\"100\" height=\"100\">";
+                answerText += "<br>";
+                answerText += "<h2>Enter the wrong answers: </h2>";
+                answerText += "<br>";
+                answerText += "Seperate the wrong answers by entering \"|\" between each answer.";
+                answerText += "<br>";
+                answerText += "<input type=\"text\" name=\"answer\" width=\"100\" height=\"100\">";
+                answerText += "<br>";
+                break;
             case "YesNo":  
-                     break;
+                answerText = "<input type=\"radio\" name=\"TrueFalse\" value=\"True\">True";
+                answerText += "<br>";
+                answerText += "<input type=\"radio\" name=\"TrueFalse\" value=\"False\">False";
+                answerText += "<br>";
+                break;
             default: 
-                     break;
+                answerText = "Please choose the type of question you would like to add.";
+                answerText += "<br>";
+                answerText += "<a href=\"AddQuestion.html\">Add New Question</a>";
+                isValid = false;
+                break;
         }
+         
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
+            
+            if (isValid)
+            {
+                out.println("<link rel=\"stylesheet\" href=\"Main.css\"/>");
+            }
+            else
+            {
+                
+            }
+            
             out.println("<title>New Question</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h2>Enter the question: </h2>");
+            
+            if (isValid)
+            {
+                out.println("<h2>Enter the question: </h2>");
+                out.println("<br>");
+                out.println("<input type=\"text\" name=\"answer\" width=\"100\" height=\"100\">");
+                out.println("<br>");
+                out.println("<h2>Enter the answer: </h2>");
+                out.println("<br>");
+            }
+            
+            out.println(answerText);
             out.println("</body>");
             out.println("</html>");
         }
